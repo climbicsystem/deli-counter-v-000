c5f1fea465d5e1d39250dd4e64077b9d3645d991
Joseph Vint <josephvint@me.com>
2018-02-11 18:40:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def line(spaces)
+  if spaces.size == 0
+    puts "The line is currently empty."
+  else
+    the_line = spaces.each_with_index.collect do |customer,index|
+      space_number = index + 1
+      "#{space_number}. #{customer}"
+    end
+    puts "The line is currently: #{the_line.join(" ")}"
+  end
+end
+
+def take_a_number
+  
+end
+
+def now_serving(spaces)
+  if spaces.size > 0
+    puts "Currently serving #{spaces[0]}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
